532622e0ff64eefc09ac6379b6e224d4d5d87ca2
evileangel <150454461+evileangel@users.noreply.github.com>
2023-11-14 15:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Xin chào Nam nha!',
+    introDesc: `Trong nắng vàng se hơi lạnh,
+    Tháng mười một đến thật nhanh
+    Có dáng người thêm một tuổi
+    Gửi lời chúc buổi hoàng hôn`,
+    btnIntro: 'Happy birthday Nam
+        (nhấp vào)',
+    title: 'Mừng sinh nhật cậu nha, chúc Nam tuổi mới đẹp lại càng đẹp thêm, tuổi mới cũng thêm nguyện vọng mới và đặc biệt nhất là đều thành công nhé 😙',
+    desc: 'Ngày hôm nay của cậu như thế nào?',
+    btnYes: 'Rất vui luôn á 😉',
+    btnNo: 'Không, thật nhạt nhẽo',
+    question:'Dù một ngày có tồi tệ đến mấy, tớ mong cậu sẽ luôn tươi cười để tin tưởng vào một ngày mai của tương lai nhé <3',
+    btnReply: 'Bất ngờ hong (gửi cho tớ nhe)',
+    reply: 'Lý Hải có tí xíu đẹp trai mà dễ thương thì siêu cấp á 😘😍',
+    mess: 'Hải biết mà 🥰. Và tớ thích... 😘😘',
+    messDesc: 'Một lần nữa, chúc cậu một năm sinh nhật đáng nhớ, một mùa nhiều kỉ niệm 😎',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.youtube.com/watch?v=bJi8SWxFJcg' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
